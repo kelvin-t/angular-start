d15953976220b066658458c04a8618fb9155c986
Tee Kai Yoong <kaiyoong.tee@gmail.com>
2021-09-17 20:03:52 +0800

    Add method 'addToCart' to product-details component class

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -27,4 +27,8 @@ export class ProductDetailsComponent implements OnInit {
     this.product = products.find(product => product.id === productIdFromRoute);
   }
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 }
